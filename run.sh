8e0ef978d2cb513a48ff580e4299795ecaeffe97
jimmyagent <jimmyagent@qq.com>
2016-06-03 10:12:11 +0800

    增加cron.sh，从sanzuwu而来

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/bin/bash
 echo 'Start crysadm on'  $(date) >> /tmp/crysadm.txt
 
@@ -13,3 +14,20 @@ sudo /etc/init.d/redis-server restart
 sudo redis-server >> /tmp/error 2>&1 &                                              
 sudo python3.4 ${BASE_DIR}/crysadm/crysadm_helper.py >> /tmp/error 2>&1 &
 sudo python3.4 ${BASE_DIR}/crysadm/crysadm.py >> /tmp/error 2>&1 & 
+=======
+#!/bin/bash
+echo 'Start crysadm on'  $(date) >> /tmp/crysadm.txt
+
+sudo pkill redis-server
+sudo pkill python3.4
+
+BASE_DIR="$( cd "$( dirname "$0"  )" && pwd  )"                                                             
+ls ${BASE_DIR}/ >> /tmp/error 2>&1
+                                     
+echo $PATH >> /tmp/error           
+echo $LD_LIBRARY_PATH >> /tmp/error                                            
+sudo /etc/init.d/redis-server restart
+sudo redis-server >> /tmp/error 2>&1 &                                              
+sudo python3.4 ${BASE_DIR}/crysadm/crysadm_helper.py >> /tmp/error 2>&1 &
+sudo python3.4 ${BASE_DIR}/crysadm/crysadm.py >> /tmp/error 2>&1 & 
+>>>>>>> 75eb7d86ac3dd3b5246f79ea8f766ded9d3c95cc
